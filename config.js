d955e88a3a5de085acbef3ea44213ce76237de14
Carlos Azaustre <cazaustre@gmail.com>
2017-04-17 20:10:23 +0200

    change config vars

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
   SECRET_TOKEN: 'miclavedetokens'
 }
