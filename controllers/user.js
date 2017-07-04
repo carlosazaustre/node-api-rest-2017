3670303d2fd40d4fe7a2b5c42435d591ea5f84b9
Edwin Castillo <eocc28@hotmail.com>
2017-07-04 15:36:12 -0500

    Autenticación con usuario y contraseña

--- controllers/user.js
+++ controllers/user.js
@@ -18,15 +18,17 @@ function signUp (req, res) {
 }
 
 function signIn (req, res) {
-  User.find({ email: req.body.email }, (err, user) => {
-    if (err) return res.status(500).send({ message: err })
-    if (!user) return res.status(404).send({ message: 'No existe el usuario' })
+  User.findOne( { email: req.body.email }, (err, user) => {
 
-    req.user = user
-    res.status(200).send({
-      message: 'Te has logueado correctamente',
-      token: service.createToken(user)
+    if (err) throw err
+
+    user.comparePassword(req.body.password, (error, isMatch) => {
+      if (!isMatch) return res.status(401).send( { message: 'Las credenciales no coinciden' } )
+
+      req.user = user
+      return res.status(200).send( { message: 'Te has logueado correctamente', token: service.createToken(user), user } )
     })
+
   })
 }
 
